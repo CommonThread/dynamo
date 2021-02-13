a09d939dcdef31cdee8e419c562b9b1d9e01866d
Tim Gosnell <timothgosnell@gmail.com>
2021-02-13 15:31:15 -0700

    returning just the one item from getItems

--- dynamo-ops.js
+++ dynamo-ops.js
@@ -11,7 +11,9 @@ export const getItem = async (docClient, tableName, field, value) => {
         }
     };
 
-    return await  docClient.scan(params).promise()
+    let result = await  docClient.scan(params).promise()
+
+    return result.Items[0]
     
 };
 
@@ -41,12 +43,16 @@ export const getItems = async (tableName, docClient) => {
  * 
  * returns just the item from dynamo resultset if available
  */
-export const getItemById = async (docClient, tableName, id, idField = 'id') => {
+export const getItemById = async (docClient, tableName, id, idField = 'id', rangeId, rangeIdField = 'rangeId') => {
 
+    let keyExp2 = ''
+    if(rangeId) {
+        keyExp2 = `and #rId=:rId`
+    }
     console.log(`getting data for item ${id}`)
     var params = {
         TableName: tableName,
-        KeyConditionExpression: '#id=:id',
+        KeyConditionExpression: `#id=:id${keyExp2}`,
         ExpressionAttributeValues: {
             ":id": id
         },
@@ -55,6 +61,11 @@ export const getItemById = async (docClient, tableName, id, idField = 'id') => {
         },
         ScanIndexForward: false
     };
+    if(rangeId){
+        params.ExpressionAttributeValues[":rId"] = rangeId
+        params.ExpressionAttributeNames["#rId"] = rangeIdField
+
+    }
     console.log(params)
     let results = await docClient.query(params).promise()
     console.log(`results: ${JSON.stringify(results)}`)
