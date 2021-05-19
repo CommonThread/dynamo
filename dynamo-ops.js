5d4c19c8017f4a4bc5da5928c0cda8ca72390076
Tim Gosnell <timothgosnell@gmail.com>
2021-05-19 07:02:05 -0600

    adding deleteItem

--- dynamo-ops.js
+++ dynamo-ops.js
@@ -73,6 +73,38 @@ export const getItemById = async (docClient, tableName, id, idField = 'id', rang
 
 }
 
+
+
+export const getItemByRangeId = async (id, idField = 'id', rangeId, rangeIdField = 'rangeId') => {
+    const docClient = new aws.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+    let table = process.env.PROCESSING_REQUEST_TABLE
+    let keyExp2 = ''
+        if(rangeId) {
+            keyExp2 = `and #rId=:rId`
+        }
+        console.log(`getting data for item ${id}`)
+        var params = {
+            TableName: tableName,
+            KeyConditionExpression: `#id=:id${keyExp2}`,
+            ExpressionAttributeValues: {
+                ":id": id
+            },
+            ExpressionAttributeNames: {
+                "#id": idField
+            },
+            ScanIndexForward: false
+        };
+        if(rangeId){
+            params.ExpressionAttributeValues[":rId"] = rangeId
+            params.ExpressionAttributeNames["#rId"] = rangeIdField
+    
+        }
+        console.log(params)
+        let results = await docClient.query(params).promise()
+        console.log(`results: ${JSON.stringify(results)}`)
+        return results.Items[0];
+    }
+
 export const insertItem = async (docClient, tableName, item) => {
     let params = {
         TableName: tableName,
@@ -115,3 +147,18 @@ export const updateItem = async (docClient, tableName, data, id, idField = 'id')
 
     return result
 }
+
+export const deleteItem = async (docClient, tableName, keys, keyValues) => {
+
+    let params = {
+      TableName: tableName,
+      Key: {}
+    };
+    params.Key[keys[0]] = keyValues[0]
+    if(keys.length > 1){
+      params.Key[keys[1]] = keyValues[1]
+    }
+  
+    console.log(params)
+    return await docClient.delete(params).promise();
+  }
